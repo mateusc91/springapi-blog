c47b5da13ca52c9e3d3cacf56786d46b6c809673
mateusc91 <mateusc.eng@gmail.com>
2023-01-25 13:15:55 -0300

    first commit

---
+++ src/main/java/com/example/springapiazure/SpringApiAzureApplication.java
@@ -0,0 +1,23 @@
+package com.example.springapiazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api")
+public class SpringApiAzureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiAzureApplication.class, args);
+    }
+
+    @GetMapping
+    public String getMessage(){
+        return "API running!";
+    }
+
+}
---
+++ src/test/java/com/example/springapiazure/SpringApiAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapiazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiAzureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
